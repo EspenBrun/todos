6a9e508a6c5f68c752341bdcad76e9c006472ae6
Espen K. Brun <espen_brun@hotmail.com>
2016-12-19 23:56:02 +0100

    Can add tasks from interface

--- todos.js
+++ todos.js
@@ -1,7 +1,24 @@
 Todos = new Mongo.Collection('todos');
 
 if(Meteor.isClient){
-	console.log('hello client');
+	Template.todos.helpers({
+		'todo': function(){
+			return Todos.find({}, {sort: {createdAt: -1}});
+		},
+	});
+
+	Template.todos.events({
+		'submit form': function(){
+			event.preventDefault();
+			var todoName = $('[name="todoName"]').val();
+			Todos.insert({
+				name: todoName,
+				completed: false,
+				createdAt: new Date()
+			});
+			$('[name="todoName"]').val('');
+		}
+	});
 }
 
 if(Meteor.isServer){
